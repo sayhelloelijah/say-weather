e5b0dbc49fffef3828cec35cef3d34d65a80c7a1
sayhelloelijah <say@helloelijah.com>
2019-05-15 23:02:14 -0500

    get weather based on location

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const xhr = new XMLHttpRequest();
-    const app = document.querySelector('#app');
 
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            let response = JSON.parse(this.responseText);
-            console.log(response);
-        }
+    var options = {
+        enableHighAccuracy: true,
+        // timeout: 5000,
+        maximumAge: 0
     };
+    
+    function success(pos) {
+        const xhr = new XMLHttpRequest();
+        let coords = pos.coords;
+        let lat = coords.latitude;
+        let lng = coords.longitude;
+        let tempContainer = document.querySelector('.weather__temp');
+        let locationContainer = document.querySelector('.weather__location');
+        
+        xhr.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                let response = JSON.parse(this.responseText);
+                let result = response.list[0];
+                let temp = Math.floor(result.main.temp);
+                let location = response.city.name;
+                console.log(response);
 
-    xhr.open('GET', 'http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={API-KEY}', true)
-    xhr.send();
+                tempContainer.innerText = temp;
+                locationContainer.innerText = location;
+
+            }
+        };
+
+        xhr.open('GET', `http://api.openweathermap.org/data/2.5/forecast?APPID=c3e37aa6de2fdb167dcd812b6d902e80&lat=${lat}&lon=${lng}&units=imperial&cnt=1`, true)   
+        xhr.send();
+    }
+
+    function error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
 });
